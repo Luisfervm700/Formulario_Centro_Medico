19bd462b3320199ae36ce2a1c5163703b9afcf81
Luis Fernando <luisfernandovm700@gmail.com>
2023-08-31 10:23:06 -0500

    Creacion de info del paciente

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,116 +1,121 @@
-import { useRef } from 'react';
+import { useState } from 'react';
 
 
 export const Formulario = () => {
 
-  const form = useRef(null)
+  const [nombreMascota, setNombreMascota] = useState('');
+  const [propietario, setPropietario] = useState('');
+  const [email, setEmail] = useState('');
+  const [fecha, setFecha] = useState('');
+  const [sintomas, setSintomas] = useState('');
 
-  const sendData = (nombreMascota, nombrePropietario, email, fecha, sintomas) => {
-    console.log(nombreMascota)
-    console.log(nombrePropietario)
-    console.log(email)
-    console.log(fecha)
-    console.log(sintomas)
+  const handleSubmit = (e) => {
+      e.preventDefault();
+
+       // 1 - Objeto de Paciente
+       const objetoPaciente = {
+          nombreMascota, 
+          propietario, 
+          email, 
+          fecha, 
+          sintomas
+      }
+      
+      // 4-RETO Validacion de datos cuando los campos o un campo esta vacio - Tarea.
+      
+      // 3-mostramos en consola los datos
+      console.log(objetoPaciente);
+
+      // 5-Reiniciar el form
+      setNombreMascota('')
+      setPropietario('')
+      setEmail('')
+      setFecha('')
+      setSintomas('')
   }
+  
 
-    const handleSubmit = () => {
-
-      const formData = new FormData(form.current);
-      const registroPaciente = {
-        'nombreMascota' : formData.get('nombreMascota'),
-        'nombrePropietario' : formData.get('nombrePropietario'),
-        'email' : formData.get('email'),
-        'fecha' : formData.get('fecha'),
-        'sintomas' : formData.get('sintomas'),
-    }
-
-      sendData(
-          registroPaciente.nombreMascota,
-          registroPaciente.nombrePropietario,
-          registroPaciente.email,
-          registroPaciente.fecha,
-          registroPaciente.sintomas,
-      )
-    }
-
-
-return (
-  <div className="w-full mx-3 px-4 flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-5">
-    {/* formulario */}
-    <form
-      ref={form}
-      className="bg-gray-100 shadow-md rounded-lg w-full md:w-1/2 p-4"
-    >
-      <div className="mb-7">
-          <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
-              Nombre Mascota
-            </label>
-            <input
-              id="nombreMascota"
-              type="text"
-              placeholder="Nombre de la Mascota"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              name="nombreMascota"
-              required
-            />
-
-          <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
-              Nombre Propietario
-            </label>
-            <input
-              id="nombrePropietario"
-              type="text"
-              placeholder="Nombre del propietario"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              name="nombrePropietario"
-              required
-            />
-
-          <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
-              Email
-            </label>
-            <input
-              id="email"
-              type="email"
-              placeholder="correo electronico"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              name="email"
-              required
-            />
-
-          <label htmlFor="fecha" className="block text-gray-700 uppercase font-bold">
-              Alta
-            </label>
-            <input
-              id="fecha"
-              type="date"
-              placeholder="fecha"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              name="fecha"
-              required
-            />
-
-          <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
-              Sintomas
-            </label>
-            <input
-              id="sintomas"
-              type="text"
-              placeholder="sintomas"
-              className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
-              name="sintomas"
-              required
-            />
-
-        </div>
-          <button 
-            type="button"
-              onClick={handleSubmit}
+  return (
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
+      {/* formulario */}
+      <form
+          onSubmit={handleSubmit} 
+          className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
+      >
+          <div className="mb-5">
+              <label htmlFor="mascota" className="block text-gray-700 uppercase font-bold">
+                  Nombre Mascota
+              </label>
+              <input
+                  id="mascota"
+                  type="text"
+                  placeholder="Nombre de la Mascota"
+                  className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                  value={nombreMascota}
+                  onChange={ (e) => setNombreMascota(e.target.value) }
+              />  
+          </div>
+  
+          <div className="mb-5">
+              <label htmlFor="propietario" className="block text-gray-700 uppercase font-bold">
+                  Nombre Propietario
+              </label>
+              <input
+                  id="propietario"
+                  type="text"
+                  placeholder="Nombre del Propietario"
+                  className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                  value={propietario}
+                  onChange={ (e) => setPropietario(e.target.value) }
+              />  
+          </div>
+  
+          <div className="mb-5">
+              <label htmlFor="email" className="block text-gray-700 uppercase font-bold">
+                  Email
+              </label>
+              <input
+                  id="email"
+                  type="email"
+                  placeholder="Email Contacto Propietario"
+                  className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                  value={email}
+                  onChange={ (e) => setEmail(e.target.value) }
+              />  
+          </div>
+  
+          <div className="mb-5">
+              <label htmlFor="alta" className="block text-gray-700 uppercase font-bold">
+                  Alta
+              </label>
+              <input
+                  id="alta"
+                  type="date"
+                  className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                  value={fecha}
+                  onChange={ (e) => setFecha(e.target.value) }
+              />  
+          </div>
+  
+          <div className="mb-5">
+              <label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">
+                  Síntomas
+              </label>
+              <textarea 
+                  id="sintomas"
+                  className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+                  placeholder="Describe los Síntomas"
+                  value={sintomas}
+                  onChange={ (e) => setSintomas(e.target.value) }
+              />
+          </div>
+  
+          <input
+              type="submit"
               className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
-            >
-              Agregar Paciente
-          </button>
-    </form>
-  </div>
-);
+              value={  'Agregar Paciente' }
+          />
+      </form>
+    </div>
+  );
 };
